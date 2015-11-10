e55f5117c593bb161412c29ffb1d3815448c3d03
Manuel Navarro <manuelnavarro1987@gmail.com>
2015-11-10 12:09:31 +0100

    Nueva interfaz de inicio!

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -12,11 +12,9 @@
     $stateProvider
       .state("home", {
         url: "/",
-        template: '<p class="lead">Welcome to the UI-Router Demo</p>' +
-          '<p>Use the menu above to navigate. ' +
-          'Pay attention to the <code>$state</code> and <code>$stateParams</code> values below.</p>' +
-          '<p>Click these links—<a href="#/c?id=1">Alice</a> or ' +
-          '<a href="#/user/42">Bob</a>—to see a url redirect in action.</p>'
+        templateProvider: function($templateCache){  
+          return $templateCache.get('interfaz/home.html'); 
+        }
       });
   }
 
--- src/app/interfaz/app.controller.js
+++ src/app/interfaz/app.controller.js
@@ -9,14 +9,19 @@
   function AppCtrl($mdSidenav, $state) {
     var vm = this;
     vm.toggleSidenav = toggleSidenav;
+    vm.closeSidenav = closeSidenav;
     vm.navigateTo    = navigateTo;
     
     function toggleSidenav(menuId) {
       $mdSidenav(menuId).toggle();
     }
 
+    function closeSidenav(menuId) {
+      $mdSidenav(menuId).close();
+    }
+
     function navigateTo(to, menuId){
-      toggleSidenav(menuId);
+      closeSidenav(menuId);
       $state.go(to);
     }
   }
